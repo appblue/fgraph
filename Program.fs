d21bbcb204b9abb7f64e7dbda76562e895a79e9d
Krzysztof Kielak <krzysztof.kielak@gmail.com>
2022-11-23 22:55:22 +0100

    added main function and description

--- Program.fs
+++ Program.fs
@@ -13,13 +13,25 @@ let rec isConnected  g s e =
         in
             [ for ed in st -> isConnected st (snd ed) e ] |> orList
 
-// definition of the directed graph. list of edges connecting nodes
-let dgraph = [
-    (1, 2); (2, 3); (2, 4); (3, 1)
-    (6, 5); (5, 4); 
-]
 
-// different test cases for isConnected
-let query = [(1, 4); (1, 5); (1, 3)]
-let res = 
-    query |> List.map (fun (x,y) -> isConnected dgraph x y)
+[<EntryPoint>]
+let main _ =
+    // definition of the directed graph. list of edges connecting nodes
+    let dgraph = [
+        (1, 2); (2, 3); (2, 4); (3, 1)
+        (6, 5); (5, 4); 
+    ]
+
+    // different test cases for isConnected
+    let query = [(1, 4); (1, 5); (1, 3)]
+
+    // results
+    let res = 
+        query |> List.map (fun (x,y) -> isConnected dgraph x y)
+
+    
+    printfn "graph: %A " dgraph
+    printfn "query: %A " query
+    printfn "result: %A " res
+
+    0
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # fgraph
-Simple DAG Algorithms in FSharp
+
+Simple DAG Algorithms in FSharp. To run execute the following command:
+
+```shell
+$ dotnet run
+```
+You should get following result:
+
+```
+graph: [(1, 2); (2, 3); (2, 4); (3, 1); (6, 5); (5, 4)] 
+query: [(1, 4); (1, 5); (1, 3)] 
+result: [true; false; true] 
+
+```
